e069debf6acc8ebb9d3d05c788c981c25b8a2ae3
asc689561 <asc689561@yahoo.com>
2018-01-08 21:50:58 +0700

    test

--- ChiecNonKyDieu/ChiecNonKyDieu/SelectGame.xaml.cs
+++ ChiecNonKyDieu/ChiecNonKyDieu/SelectGame.xaml.cs
@@ -21,17 +21,50 @@ namespace ChiecNonKyDieu
     public partial class SelectGame : UserControl
     {
         public event EventHandler<EventArgs> Done;
+
         public SelectGame()
         {
             InitializeComponent();
         }
 
+        public List<string> numberPlayerList = new List<string>()
+        {
+            "1 Nhóm chơi",
+            "2 Nhóm chơi",
+            "3 Nhóm chơi",
+            "4 Nhóm chơi",
+
+        };
+        public List<string> classList = new List<string>()
+        {
+            "Khối 1",
+            "Khối 2",
+            "Khối 3",
+            "Khối 4",
+            "Khối 5"
+        };
+
+        int cbbNumberPlayerIndex { get; set; } = 0;
+        int classIndex { get; set; } = 0;
+
         private void PlayClick(object sender, RoutedEventArgs e)
         {
-            StaticData.Khoi = (cbbKhoi.SelectedItem as ComboBoxItem).Tag.ToString();
-            StaticData.SoNguoiChoi = int.Parse((cbbSoNguoiChoi.SelectedItem as ComboBoxItem).Tag.ToString());
-            this.Visibility = Visibility.Hidden;
-            Done?.Invoke(null, null);
+            //StaticData.Khoi = (cbbKhoi.SelectedItem as ComboBoxItem).Tag.ToString();
+            //StaticData.SoNguoiChoi = int.Parse((cbbSoNguoiChoi.SelectedItem as ComboBoxItem).Tag.ToString());
+            //this.Visibility = Visibility.Hidden;
+            //Done?.Invoke(null, null);
+        }
+
+        private void NumberPlayerClick(object sender, RoutedEventArgs e)
+        {
+            cbbNumberPlayerIndex++;
+            btnNumberPlayer.Content = numberPlayerList[cbbNumberPlayerIndex % numberPlayerList.Count];
+        }
+
+        private void ClassClick(object sender, RoutedEventArgs e)
+        {
+            classIndex++;
+            btnClass.Content = classList[classIndex % classList.Count];
         }
     }
 }
